e29db858d466acea61e583722fa4d25c78aea9f5
Álvaro Torres Cogollo <atorrescogollo@gmail.com>
2020-04-12 22:49:49 +0200

    Userdata script added and moved pip requirements for virtualenv

---
+++ tests/ec2-userdata.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -xe
+
+TMP=$(mktemp -d)
+#trap "{ rm -rf $TMP; }" EXIT
+
+yum install -y \
+  python3 \
+  python3-pip \
+  git-core
+
+# Prepare virtualenv & activate
+python3 -m venv $TMP/venv
+. $TMP/venv/bin/activate
+
+# Install ansible , download role and install requirements
+pip3 install ansible
+git clone https://github.com/HackThisCompany/DC-9-ansible.git $TMP/DC-9-ansible
+pip3 install -r $TMP/DC-9-ansible/tests/requirements.txt
+
+# Prepare playbook
+cat << EOF > $TMP/playbook.yml
+- name: 'Provide DC-9 server'
+  hosts: localhost
+  become: yes
+  connection: local
+  roles:
+    - role: $TMP/DC-9-ansible
+EOF
+
+# Run playbook
+ansible-playbook -i localhost, $TMP/playbook.yml
